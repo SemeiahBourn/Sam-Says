55262bebb1fc188ab2994d78f862d5d4f12fa738
Semeiah Bourn <semeiahbourn@MacBook-Pro-2.local>
2022-05-16 09:15:06 -0400

    deploy test

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 # wire frame
 https://miro.com/app/board/uXjVO2AY03o=/
 
+# source code 
+https://www.udemy.com/course/the-complete-web-development-bootcamp/learn/lecture/12664354#overview
+
+# Link to Hosted site
+https://github.com/SemeiahBourn/Sam-says
+
+
+# How to Play
+Select any key to begin
+Repeat the sequence provided by computer
+If you enter the sequence incorrectly you lose
+
+
+# Improvements
+I could not get the purple and pink key to make sound. That needs work.
+Cutting down on my life of code would be better.
+Graphics could be improved.
--- game.js
+++ game.js
@@ -8,17 +8,19 @@ var buttonColours = ["purple", "blue", "pink", "yellow"];
 
 var gamePattern = [];
 var userClickedPattern = [];
-
+//This allows the game to be determined and identified as started or not. 
 var started = false;
 
 
-// // The levels have to start at somewhere so there you have it.
+//  The levels have to start at somewhere so there you have it.
 var level = 0;
 
-// // This targeting the entire document using JQuery. Then we used a event of keypress to start the game.
+var timeout = 1000;
+
+// This targeting the entire document using JQuery. Then we used a event of keypress to start the game.
 $(document).keypress(function() {
   if (!started) {
-      // if it's fale aka not started we will target the title using the specified id and insert the text below
+      // if not false aka not started we will target the title using the specified id and insert the text below
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
@@ -36,39 +38,77 @@ $(".btn").click(function() {
   checkAnswer(userClickedPattern.length-1);
 });
 
+// There must be a way to ensure the correct button was pushed in order for it to actually a game.
 function checkAnswer(currentLevel) {
-
+//If the game pattern and the the pattern entered is equal to the same thing on that specific level..
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      // ALSO if the length(number of button selected) of each are equal
       if (userClickedPattern.length === gamePattern.length){
+        // run the function below to give the next sequence/fucntion
         setTimeout(function () {
           nextSequence();
-        }, 1000);
+          // at this speed
+        }, timeout);
       }
+      // Otherwise they got it wrong
     } else {
+      //You got it wrong so now you get the loser sound
       playSound("wrong");
+      // This JQuery code is targeting the body and adding game over to it.
       $("body").addClass("game-over");
+      // This Jquery line is targeting the specific id and adding this text below.
       $("#level-title").text("Game Over, Press Any Key to Restart");
-
+// The same function used prior is being ran and instead of providing the next sequence it is removing the text "game-over" from the body.
       setTimeout(function () {
         $("body").removeClass("game-over");
-      }, 200);
-
+      //This is the speed of the timeout function
+      }, 1000);
+// The startover function is being called to initate the start the game over.
       startOver();
     }
 }
 
-
+//Create a function to allow for random sequences to be generated.
 function nextSequence() {
   userClickedPattern = [];
+  // This is the increment at which the the levels will increase by
   level++;
+//bonus stuff below
+
+if (level === 7  ) {
+  popUp();
+  addButton();
+  speedItUp();
+} else if (level === 5  ){
+  addButton();
+  speedItUp();
+} else if (level === 3){
+  speedItUp();
+}  else {
+  
+}
+//  console.log(nextSequence());
+
+  
+
+
+
+
+  // This JQuery line of code is targeting the specific id and adding the following text to it.
   $("#level-title").text("Level " + level);
-  var randomNumber = Math.floor(Math.random() * 4);
+  // Create a random number between 0-3 and store it in a variable. It is between 0-3 because there are four colors/buttons to choose from. 
+  var randomNumber = Math.floor(Math.random()*buttonColours.length);
+  // This variable was created to assign a random button in random sequences.
   var randomChosenColour = buttonColours[randomNumber];
+  // .push will continue to generate random patterns to the array in the variable above.
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
+  console.log(gamePattern);
 }
+
+
 
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
@@ -82,8 +122,38 @@ function playSound(name) {
   audio.play();
 }
 
+
+//This function was created to reset the game/end the game
 function startOver() {
+  //The game will restart at zero
   level = 0;
+
   gamePattern = [];
+console.log(" yeehaw " , gamePattern)
+const orangeButton = document.querySelector('.orange')
+orangeButton.style.display = 'none';
+
+  // This was set in a variable prior to symbolize the game not started.
   started = false;
 }
+//This function will change the speed of that the sequence is called.
+function speedItUp(){
+timeout =  (timeout * 2 )
+console.log("current timeout speed is " + timeout);
+}
+
+// This function will add a button to the array once a certain level is created
+function addButton(){
+  const orangeButton = document.querySelector('.orange')
+  orangeButton.style.display = 'inline-block';
+  gamePattern.push("orange")
+}
+
+
+// function popUp(){
+// const imgScary = document.querySelector('.img')
+// imgScary.style.display = 'inline-block';
+
+// }
+
+
